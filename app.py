9cdd8c3cf4a9ce3ea2e7f7bf22ea61215f62fe51
Afif Malghani <afifulhasnain@gmail.com>
2023-12-26 15:36:50 +0500

    backend

---
+++ app.py
@@ -0,0 +1,194 @@
+from flask import Flask, request, make_response, render_template
+from flask_restful import Api, Resource
+from flask_cors import CORS
+import requests
+from bs4 import BeautifulSoup
+# from utils import getUserAllergensIDs, getAllergensNames, getProductIngredients, getAllergens, addUserAllergens, removeUserAllergens, getUserRole, addUser, addAllergensFn, searchAllergensFn
+from utils import *
+
+
+class HelloWorld(Resource):
+    def get(self):
+        # render index.html 
+        return make_response(render_template('index.html'))
+    
+    def post(self):
+        request.json['user_id']
+        role = getUserRole(request.json['user_id'])
+        if role == None:
+            role = addUser(request.json['user_id'])
+        
+        return {'message': 'success', 'role': role}
+
+class CheckAllergies(Resource):
+    def post(self):
+        try:
+            user_id = request.json['user_id']
+            product_link = request.json['product_link']
+        except:
+            print(request.json)
+            return {'status': 'error'}
+        
+        
+        print(request.json)
+        user_allergies = getUserAllergensIDs(user_id)
+        
+        if len(user_allergies) > 0:
+            
+            # if product_link is a list, check all of them
+            if isinstance(product_link, list):
+                # print("got list")
+                allergic_to_products = []
+                # print(user_allergies)
+                for link in product_link:
+                    allergen_names = getAllergensNames(user_allergies)
+                    
+                    if len(allergen_names) > 0:
+                        
+                        # get the product page html
+                        # url = "https://www.carrefouruae.com/mafuae/en/"+ link
+                        
+                        
+                        ingredientsList = getProductIngredients(link)
+                        
+                        # print(ingredientsList)
+                        
+                        for ingredient in ingredientsList:
+                            for allergen in allergen_names:
+                                # print("checking for {} in {}".format(allergen, ingredient))
+                                if allergen.lower() in ingredient.lower():
+                                    allergic_to_products.append(link)
+                                    # print(allergic_to_products)
+                                    print('allergen found') 
+                                else:
+                                    pass
+                                               
+                    else:
+                        return {'status': 'no allergies'}
+                print("returning response: ", allergic_to_products)
+                return {'status': 'allergies', 'allergies': allergic_to_products}
+            elif isinstance(product_link, str):
+                allergen_names = getAllergensNames(user_allergies)
+                
+                allergens_present = []
+                if len(allergen_names) > 0:
+                    
+                    # get the product page html
+                    url = "https://www.carrefouruae.com/mafuae/en/"+ product_link
+                    
+                    
+                    ingredientsList = getProductIngredients(url)
+                    
+                    for ingredient in ingredientsList:
+                        for allergen in allergen_names:
+                            print("checking for {} in {}".format(allergen, ingredient))
+                            if allergen.lower() in ingredient.lower():
+                                allergens_present.append(allergen)
+                                print(allergens_present)
+                                print('allergen found') 
+                                return {'status': 'allergies', 'allergies': allergens_present}
+                            else:
+                                pass
+                    return {'status': 'no allergies'}
+                                           
+                else:
+                    return {'status': 'no allergies'}
+        else:
+            return {'status': 'no allergies'}
+        
+
+class saveAllergies(Resource):
+    def post(self):
+        # get user id and allergies
+        try:
+            user_id = request.json['user_id']
+            allergies = request.json['allergies']
+        except:
+            print(request.json)
+            return {'status': 'error'}
+        
+        # get user allergies
+        
+        print(allergies)
+        
+        print(user_id)
+        
+        user_allergies = getUserAllergensIDs(user_id)
+        
+        print(user_allergies)
+        
+        allergies = [int(x) for x in allergies]
+        
+        toAdd = []
+        toRemove = []
+        
+        # if an item is in allergeis but not in user_allergies, add it
+        # if an item is in user_allergies but not in allergies, remove it
+        for allergy in allergies:
+            if allergy not in user_allergies:
+                toAdd.append(allergy)
+        for user_allergy in user_allergies:
+            if user_allergy not in allergies:
+                toRemove.append(user_allergy)
+        
+        # add new allergies
+        addUserAllergens(user_id, toAdd)
+        
+        # remove old allergies
+        removeUserAllergens(user_id, toRemove)
+        
+        
+        
+        return {'status': 'success'}
+    
+
+class getList(Resource):
+    
+    # recieve post reuqeust with user id
+    def post(self):
+        user_id = request.json['user_id']
+        
+        allergens = getAllergens(user_id)
+        
+        # return list of allergen names
+        return {'allergens': allergens}
+    
+
+class addAllergens(Resource):
+    def post(self):
+        allergensList = request.json['allergens']
+        addAllergensFn(allergensList)
+        return {'message': 'success'}
+
+
+class searchAllergens(Resource):
+    def post(self):
+        allergen = request.json['allergen']
+        allergens = searchAllergensFn(allergen)
+        return {'message':"success",  'allergens': allergens}
+        
+        
+class removeAllergens(Resource):
+    def post(self):
+        allergenIDs = request.json['allergens']
+        removeAllergensFn(allergenIDs)
+        return {'message': 'success'}
+
+
+
+app = Flask(__name__)
+api = Api(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+api.add_resource(HelloWorld, '/')
+api.add_resource(CheckAllergies, '/allergies')
+api.add_resource(saveAllergies, '/settings')
+api.add_resource(getList, '/allergens-all')
+api.add_resource(addAllergens, '/add-allergens')
+api.add_resource(searchAllergens, '/search-allergens')
+api.add_resource(removeAllergens, '/remove-allergens')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,275 @@
+import mysql.connector
+import requests
+from bs4 import BeautifulSoup
+
+def connectDB():
+    cnx = mysql.connector.connect(user='test', password='Test123!',
+                                host='localhost',
+                                database='allergen_blocker')
+    return cnx
+
+
+
+def getAllergens(user_id):
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    query = ("SELECT * FROM allergens")
+    cursor.execute(query)
+    result = cursor.fetchall()
+    
+    cnx.commit()
+    cnx.close()
+    
+    allergens = {}
+    if len(result) == 0:
+        return allergens
+    
+    for row in result:
+        allergens[row[0]] = row[1]
+    
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    query = ("SELECT * FROM user_allergens where user_id = %s")
+    cursor.execute(query, (user_id,))
+    result = cursor.fetchall()
+    
+    cnx.commit()
+    cnx.close()
+    
+    if len(result) == 0:
+        return allergens
+    
+    for row in result:
+        allergens[row[1]] = allergens[row[1]] + " (selected)"
+    
+    return allergens
+
+
+
+def getUserAllergensIDs(user_id):
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    query = ("SELECT * FROM user_allergens WHERE user_id = %s")
+    cursor.execute(query, (user_id,))
+    result = cursor.fetchall()
+    
+    cnx.commit()
+    cnx.close()
+    
+    allergen_ids = []
+    
+    if len(result) == 0:
+        return allergen_ids
+    
+    for row in result:
+        allergen_ids.append(row[1])
+    
+    return allergen_ids
+
+
+
+def getAllergensNames(allergen_ids):
+    allergen_names = []
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    for allergen_id in allergen_ids:
+        cnx = connectDB()
+        cursor = cnx.cursor()
+        query = ("SELECT allergen_name FROM allergens WHERE allergen_id = %s")
+        cursor.execute(query, (allergen_id,))
+        result = cursor.fetchall()
+        cnx.commit()
+        cnx.close()
+        if len(result) > 0:
+            allergen_names.append(result[0][0])
+        else:
+            pass
+    
+    
+    return allergen_names
+
+
+def getProductIngredients(url):
+    
+    ingredientsList = []
+    
+    page = requests.get(url)
+                
+    # print(page.text)
+    
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    # find the t
+    ingredient_tag = soup.find(text='Ingredients')
+    
+    
+    
+    print("Ingredients Tag: ", ingredient_tag)
+    
+    
+    
+    if ingredient_tag:
+        
+        parentHtml = ingredient_tag.find_parent()
+        
+        if not parentHtml:
+            return ingredientsList
+        
+        parentDiv = parentHtml.find_parent()
+        
+        if not parentDiv:
+            return ingredientsList
+        
+        
+        # find the child div and get its contents
+        child_div = parentDiv.find('div')
+        
+        
+        if child_div:
+            # replace all span elements with a comma
+            for span in child_div.find_all('span'):
+                span.replace_with(', ')
+            
+            ingredientsList = child_div.get_text(strip=True).split(',')
+    
+    return ingredientsList
+
+def addUserAllergens(user_id, allergens):
+    for allergen in allergens:
+        # add allergen to user
+        cnx = connectDB()
+        cursor = cnx.cursor()
+        
+        query = ("INSERT INTO user_allergens (user_id, allergen_id) VALUES (%s, %s)")
+        cursor.execute(query, (user_id, allergen))
+        
+        cnx.commit()
+        cnx.close()
+        
+        print('allergen added')
+    return True
+
+
+def removeUserAllergens(user_id, allergens):
+    for allergen in allergens:
+        # remove allergen from user
+        
+        print(allergen)
+        cnx = connectDB()
+        cursor = cnx.cursor()
+        
+        query = ("DELETE FROM user_allergens WHERE user_id = %s AND allergen_id = %s")
+        cursor.execute(query, (user_id, allergen))
+        
+        cnx.commit()
+        cnx.close()
+        
+        print('allergen removed')
+    return True
+
+def getUserRole(user_id):
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    query = ("SELECT role FROM user_roles WHERE user_id = %s")
+    cursor.execute(query, (user_id,))
+    result = cursor.fetchall()
+    
+    cnx.commit()
+    cnx.close()
+    
+    if len(result) == 0:
+        return None
+    
+    return result[0][0]
+
+
+def addUser(user_id):
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    print("adding user", user_id)
+    
+    # add user to users table with name set to default
+    query = ("INSERT INTO users (user_id) VALUES (%s)")
+    cursor.execute(query, (user_id,))
+    
+    cnx.commit()
+    cnx.close()
+    
+    print("user added")
+    
+    # add user to user_roles table with role set to user
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    query = ("INSERT INTO user_roles (user_id, role) VALUES (%s, %s)")
+    cursor.execute(query, (user_id, 'user'))
+    
+    cnx.commit()
+    cnx.close()
+    
+    # return the role assigned
+    return 'user'
+
+
+
+def addAllergensFn(allergens):
+    for allergen in allergens:
+        # add allergen to user
+        cnx = connectDB()
+        cursor = cnx.cursor()
+        
+        query = ("INSERT INTO allergens (allergen_name) VALUES (%s)")
+        cursor.execute(query, (allergen,))
+        
+        cnx.commit()
+        cnx.close()
+        
+        print('allergen added')
+    return True
+
+
+def searchAllergensFn(allergen_name):
+    cnx = connectDB()
+    cursor = cnx.cursor()
+    
+    # search with like query with % on either side
+    query = ("SELECT * FROM allergens WHERE allergen_name LIKE %s")
+    cursor.execute(query, ('%'+allergen_name+'%',))
+    result = cursor.fetchall()
+    
+    cnx.commit()
+    cnx.close()
+    
+    if len(result) == 0:
+        return False
+    
+    # make a dict of allergens
+    allergens = {}
+    for row in result:
+        allergens[row[0]] = row[1]
+        
+        
+    return allergens
+
+
+def removeAllergensFn(allergen_ids):
+    print(allergen_ids)
+    for allergen_id in allergen_ids:
+        # remove allergen from user
+        cnx = connectDB()
+        cursor = cnx.cursor()
+        
+        query = ("DELETE FROM allergens WHERE allergen_id = %s")
+        cursor.execute(query, (allergen_id,))
+        
+        cnx.commit()
+        cnx.close()
+        
+        print('allergen removed')
+    return Trueit
